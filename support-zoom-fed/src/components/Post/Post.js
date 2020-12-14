95b2c04c8bf50cb544da03b048d08c37648ab46d
Rich T <richard.todd@shopify.com>
2020-12-14 15:03:20 -0500

    React micropost feed setup

---
+++ support-zoom-fed/src/components/Post/Post.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import * as styles from './styles.module.scss';
+
+class Post extends Component {
+  render() {
+    return (
+      <li className={styles.Post}>
+        <img className={styles.Gravatar} src="" alt="user gravatar" />
+        <span className={styles.PostInner}>
+          <span className={styles.User}><a href="/">Name</a></span>
+          <span className={styles.Content}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at mollis orci, et fringilla est. Mauris a
+            nunc nec tortor dapibus dictum. Suspendisse laoreet, lorem a laoreet consequat, turpis augue volutpat neque,
+            at mollis dui dolor at leo. Curabitur aliquet mollis metus nec mollis. Nam lobortis ultricies arcu, eget
+            egestas turpis consequat ac.
+          </span>
+          <span className={styles.Tag}>
+            <em>#tag</em>
+          </span>
+          <span className={styles.Timestamp}>Posted 4 days ago.</span>
+        </span>
+      </li>
+    );
+  }
+}
+
+export default Post;
