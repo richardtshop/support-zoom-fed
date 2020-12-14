6d401d828c9d06dff6ac6a9af3e6314ddbfd2c41
Rich T <richard.todd@shopify.com>
2020-12-14 16:52:29 -0500

    Added routes

--- src/foundation/App/App.js
+++ src/foundation/App/App.js
@@ -1,13 +1,25 @@
+
+
+
+import React,  { useState } from 'react';
+
 import '../../styles/App.scss';
 import { Nav, Footer } from './components';
-import { User } from '../../features';
+import Routes from '../Routes';
 
 function App() {
+  const [count, updateCount] = useState(0);
+
+  const handleClick = () => {
+    updateCount(count + 1);
+  };
+
   return (
     <div className="app">
       <Nav />
       <main>
-        <User />
+        <button onClick={handleClick}>{count}</button>
+        <Routes />
       </main>
       <Footer />
     </div>
--- src/foundation/App/components/Nav/Nav.js
+++ src/foundation/App/components/Nav/Nav.js
@@ -1,31 +1,41 @@
 import React from 'react';
+import * as styles from './nav.module.scss';
 
-function Nav(){
-    return (
-      <header className="app-header">
-        <div className="app-header--inner">
-          <a id="logo" href="/">Curds and Whey 🧀</a>
-          <nav className="nav">
-            <ul className="nav-links">
-              <li>
-                <a href="#" className="app-link">Home</a>
-              </li>
+function Nav() {
+  return (
+    <header className={styles.AppHeader}>
+      <div className={styles.AppHeaderInner}>
+        <a id="logo" href="/">
+          Curds and Whey 🧀
+        </a>
+        <nav className="nav">
+          <ul className="nav-links">
+            <li>
+              <a href="/" className="app-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="app-link">
+                About
+              </a>
+            </li>
 
-              <li>
-                <a href="#" className="app-link">About</a>
-              </li>
-
-              <li>
-                <a href="#" className="app-link">Log in</a>
-              </li>
-              <li>
-                <a href="#" className="app-link btn">New post</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
+            <li>
+              <a href="#" className="app-link">
+                Log in
+              </a>
+            </li>
+            <li>
+              <a href="#" className={`app-link ${styles.NavBtn}`}>
+                New post
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Nav;
