12d3bac28cd9aaae36e4a6db7f83b373d15337fb
Rich T <richard.todd@shopify.com>
2020-12-14 15:48:52 -0500

    Converted class components to functional components

--- src/components/PostFeed/PostFeed.js
+++ src/components/PostFeed/PostFeed.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as styles from './styles.module.scss';
 import Post from '../Post';
 
-class PostFeed extends Component {
-  render() {
-    const posts = [1, 1, 1, 1, 1, 1];
+function PostFeed() {
+  const posts = [1, 1, 1, 1, 1, 1];
 
-    return (
-      <div className={styles.PostFeed}>
-        <h2>Whitter feed</h2>
-        <ul>
-          {posts.map(() => (
-            <Post />
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.PostFeed}>
+      <h2>Whitter feed</h2>
+      <ul>
+        {posts.map(() => (
+          <Post />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PostFeed;
--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as styles from './styles.module.scss';
 
-class UserInfo extends Component {
-  render() {
-    return (
-      <div className={styles.UserInfo}>
-        <section className="user-info">
-          <img className="gravatar" src="" alt="user gravatar" />
+function UserInfo({ user }) {
+  const { name, microposts, following, followers } = user;
+  return (
+    <div className={styles.UserInfo}>
+      <section className="user-info">
+        <img className="gravatar" src="" alt="user gravatar" />
 
+        <span>
+          <h1>{name}</h1>
           <span>
-            <h1>{this.props.user.name}</h1>
-            <span>
-              <a href="/">View my profile</a>
-            </span>
-            <span>{this.props.user.microposts.length} microposts</span>
+            <a href="/">View my profile</a>
           </span>
-        </section>
-        <section className={styles.UserStats}>
-          <a href="/">
-            <strong id="following" className="stat">
-              {this.props.user.following}
-            </strong>
-            following
-          </a>
-          <a href="/">
-            <strong id="followers" className="stat">
-              {this.props.user.followers}
-            </strong>
-            followers
-          </a>
-        </section>
-      </div>
-    );
-  }
+          <span>{microposts.length} microposts</span>
+        </span>
+      </section>
+      <section className={styles.UserStats}>
+        <a href="/">
+          <strong id="following" className="stat">
+            {following}
+          </strong>
+          following
+        </a>
+        <a href="/">
+          <strong id="followers" className="stat">
+            {followers}
+          </strong>
+          followers
+        </a>
+      </section>
+    </div>
+  );
 }
 
 export default UserInfo;
