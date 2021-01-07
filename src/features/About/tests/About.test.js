d71d9261ce5725715c917f43e3250d592b111228
Rich T <richard.todd@shopify.com>
2021-01-07 17:09:14 -0500

    Test setup

---
+++ src/features/About/tests/About.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import About from '../About';
+import { mount } from '@shopify/react-testing';
+import '@shopify/react-testing/matchers';
+
+describe('<About />', () => {
+  it('has an h2 element with text content react', () => {
+    const wrapper = mount(<About/>);
+    expect(wrapper).toContainReactHtml('<h2>About</h2>');
+  });
+  
+  
+  it('has an h2 element with text content', () => {
+    const wrapper = mount(<About/>);
+    expect(wrapper.find('h2').text()).toBe('About');
+  });
+});
+
